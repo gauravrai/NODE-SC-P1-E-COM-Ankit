0fc37c138809f84ad63266433f6e910b412f610c
krsonali2607 <69144451+krsonali2607@users.noreply.github.com>
2020-10-08 19:26:49 +0530

    wishlist api

--- controllers/api/wishlist/index.controller.js
+++ controllers/api/wishlist/index.controller.js
@@ -6,31 +6,139 @@ const mongoose = require('mongoose');
 const bcrypt = require("bcrypt-nodejs");
 const moment = require('moment');
 const Wishlist = model.wishlist;
+const { validationResult } = require('express-validator');
 
 module.exports = {
-    // add wishlist 
-    addwishlist:async function(req,res){
-        var productId = req.body.productId;
-        var userId = req.body.userId;
-
-        if (productId ==null || productId == '')
-        {
-            return res.status(400).json({ message: "Product Id is Not Empty" });
-        } 
-        if (userId ==null || userId == '')
-        {
-            return res.status(400).json({ message: "User ID is Not Empty" });
-        } 
-
-        let wishlistData = {
-            productId : productId,
-            userId  : userId
-        };
-        let wishlist = new Wishlist(wishlistData);
-        wishlist.save(function(err, data){
-            if(err){console.log(err)}
-            
-            return res.status(200).json({ data:data, status: 'success', message: "Wishlist  Add successfully!!"});
-        })      
+    // @route       GET api/v1/addWishlist
+    // @description User request for product
+    // @access      Public
+    addWishlist : async function(req,res){
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        try{
+            let wishlistData = {
+                userId : mongoose.mongo.ObjectId(req.body.userId),
+                productId : mongoose.mongo.ObjectId(req.body.productId)
+			};
+			let wishlist = new Wishlist(wishlistData);
+			wishlist.save(function(err, data){
+				return res.status(200).json({ 
+                    data: wishlistData, 
+                    status: 'success', 
+                    message: "Wishlist added successfully!!" 
+                });	
+			})
+        }
+        catch (e){
+            console.log(e)
+            return res.status(500).json({ 
+                                    data: [],  
+                                    status: 'error', 
+                                    errors: [{
+                                        msg: "Internal server error"
+                                    }]
+                                });
+        }
+    },
+    // @route       GET api/v1/getWishlistOfUser
+    // @description Search product
+    // @access      Public
+    getWishlistOfUser : async function(req,res){
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        try{
+            let pageno = req.query.pageno ? parseInt(req.query.pageno) : config.constant.PAGENO;
+            let limit = config.constant.LIMIT;
+            let skip = (pageno-1) * limit;
+            var userId = req.query.userId;
+            var wishlistData = await Wishlist.aggregate([ 
+                {
+                    $match : { userId: mongoose.mongo.ObjectID(req.query.userId), status: true, deletedAt: 0 }
+                },
+                {
+                    $lookup:
+                      {
+                        from: "products",
+                        localField: "productId",
+                        foreignField: "_id",
+                        as: "productData"
+                      }
+                },
+                {
+                    $addFields: {
+                        "thumbnailPath" : config.constant.PRODUCTTHUMBNAILSHOWPATH,
+                        "smallPath" : config.constant.PRODUCTSMALLSHOWPATH,
+                        "largePath" : config.constant.PRODUCTLARGESHOWPATH
+                    }
+                },
+                {
+                    $project: { 
+                        __v:0,
+                        createdAt:0,
+                        updatedAt:0
+                    }
+                }
+            ]).skip(skip).limit(limit);
+            if(wishlistData.length>0) {
+                return res.status(200).json({ 
+                                            data: wishlistData, 
+                                            status: 'success', 
+                                            message: "Data fetched successfully!!" 
+                                        });
+            } else {
+                return res.status(400).json({ 
+                                            data: [], 
+                                            status: 'error', 
+                                            message: "No Data Found!!" 
+                                        });
+            }
+        }
+        catch (e){
+            console.log(e)
+            return res.status(500).json({ 
+                                    data: [],  
+                                    status: 'error', 
+                                    errors: [{
+                                        msg: "Internal server error"
+                                    }]
+                                });
+        }
+    },
+    // @route       GET api/v1/removeWishlist
+    // @description User request for product
+    // @access      Public
+    removeWishlist : async function(req,res){
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        try{
+            // let wishlistData = {
+            //     userId : mongoose.mongo.ObjectId(req.body.userId),
+            //     productId : mongoose.mongo.ObjectId(req.body.productId)
+			// };
+			// let wishlist = new Wishlist(wishlistData);
+			// wishlist.save(function(err, data){
+			// 	return res.status(200).json({ 
+            //         data: wishlistData, 
+            //         status: 'success', 
+            //         message: "Wishlist removed successfully!!" 
+            //     });	
+			// })
+        }
+        catch (e){
+            console.log(e)
+            return res.status(500).json({ 
+                                    data: [],  
+                                    status: 'error', 
+                                    errors: [{
+                                        msg: "Internal server error"
+                                    }]
+                                });
+        }
     },
 }
--- routes/api/wishlist.routes.js
+++ routes/api/wishlist.routes.js
@@ -1,11 +1,29 @@
 var indexController = require('../../controllers/api/wishlist/index.controller');
 var config = require('../../config/index');
+const { check } = require('express-validator')
 
 module.exports = function(router) {
-      router.all(config.constant.APIURL+'/addwishlist', indexController.addwishlist); 
-    //  router.post(config.constant.APIURL+'/checkcustomerotp',  indexController.checkCustomerOtp);
-    //  router.get(config.constant.APIURL+'/customer_profile',  indexController.customerProfile);
-    //  router.post(config.constant.APIURL+'/update_customerprofile',  indexController.updateCustomerProfile);
-
-    
+  router.post(
+		config.constant.APIURL+'/addWishlist', 
+		[
+      check('productId', 'Product Id is required').not().isEmpty(),
+      check('userId', 'User Id is required').not().isEmpty()
+		], 
+		indexController.addWishlist
+	); 
+  router.get(
+    config.constant.APIURL+'/getWishlistOfUser', 
+    [
+      check('userId', 'User Id is required').not().isEmpty()
+    ], 
+    indexController.getWishlistOfUser
+  );    
+  router.post(
+    config.constant.APIURL+'/removeWishlist', 
+    [
+      check('productId', 'Product Id is required').not().isEmpty(),
+      check('userId', 'User Id is required').not().isEmpty()
+    ], 
+    indexController.removeWishlist
+  );     
 }
