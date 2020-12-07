d19cd6cea0276743eb3b59aa0dd1da4c5c41116d
krsonali2607 <69144451+krsonali2607@users.noreply.github.com>
2020-12-07 22:59:04 +0530

    same as billing address issue

--- controllers/api/cart/index.controller.js
+++ controllers/api/cart/index.controller.js
@@ -354,7 +354,7 @@ module.exports = {
             data.address = userData.address ? userData.address : '';
             data.mobile = userData.mobile ? userData.mobile : '';
             data.gst = userData.gst ? userData.gst : '';
-            data.sameAsBillingAddress = userData.sameAsBillingAddress ? userData.sameAsBillingAddress : '';
+            data.sameAsBillingAddress = userData.sameAsBillingAddress ? userData.sameAsBillingAddress : false;
 
             data.billingAddress.address = userData.billingAddress.address ? userData.billingAddress.address : '';
             data.billingAddress.country = userData.billingAddress.country ? userData.billingAddress.country : '';
